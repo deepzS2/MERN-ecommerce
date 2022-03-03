3c7abf2d14bedce2f3eacbb94c0cc202dd205eb1
deepzS2 <alanbackman10@hotmail.com>
2022-03-03 15:33:33 -0300

    ✨(frontend): Sidebar added!

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,11 +1,95 @@
 import React from 'react'
+import {
+	MdLineStyle,
+	MdTimeline,
+	MdTrendingUp,
+	MdPermIdentity,
+	MdStorefront,
+	MdBarChart,
+	MdMailOutline,
+	MdDynamicFeed,
+	MdChatBubbleOutline,
+	MdWorkOutline,
+	MdReport,
+} from 'react-icons/md'
 
-import { Container } from './styles'
+import { Container, Menu, Wrapper } from './styles'
 
 const Sidebar: React.FC = () => {
+	const iconsSize = 20
+
 	return (
 		<Container>
-			<h1>Sidebar</h1>
+			<Wrapper>
+				<Menu>
+					<h3>Dashboard</h3>
+					<ul>
+						<li>
+							<MdLineStyle size={iconsSize} />
+							Home
+						</li>
+						<li>
+							<MdTimeline size={iconsSize} />
+							Analytics
+						</li>
+						<li>
+							<MdTrendingUp size={iconsSize} />
+							Sales
+						</li>
+					</ul>
+				</Menu>
+				<Menu>
+					<h3>Quick Menu</h3>
+					<ul>
+						<li>
+							<MdPermIdentity size={iconsSize} />
+							Users
+						</li>
+						<li>
+							<MdStorefront size={iconsSize} />
+							Analytics
+						</li>
+						<li>
+							<MdBarChart size={iconsSize} />
+							Reports
+						</li>
+					</ul>
+				</Menu>
+				<Menu>
+					<h3>Notifications</h3>
+					<ul>
+						<li>
+							<MdMailOutline size={iconsSize} />
+							Mail
+						</li>
+						<li>
+							<MdDynamicFeed size={iconsSize} />
+							Feedback
+						</li>
+						<li>
+							<MdChatBubbleOutline size={iconsSize} />
+							Messages
+						</li>
+					</ul>
+				</Menu>
+				<Menu>
+					<h3>Staff</h3>
+					<ul>
+						<li>
+							<MdWorkOutline size={iconsSize} />
+							Manage
+						</li>
+						<li>
+							<MdTimeline size={iconsSize} />
+							Analytics
+						</li>
+						<li>
+							<MdReport size={iconsSize} />
+							Reports
+						</li>
+					</ul>
+				</Menu>
+			</Wrapper>
 		</Container>
 	)
 }
--- frontend/src/components/Sidebar/styles.ts
+++ frontend/src/components/Sidebar/styles.ts
@@ -1,3 +1,46 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.article`
+	flex: 1;
+	height: calc(100vh - 50px);
+	position: sticky;
+	top: 50px;
+	background-color: rgb(251, 251, 255);
+`
+
+export const Wrapper = styled.div`
+	padding: 20px;
+	color: ${({ theme }) => theme.colors.grey};
+`
+
+export const Menu = styled.div`
+	margin-bottom: 10px;
+
+	h3 {
+		font-size: 13px;
+		color: rgb(187, 186, 186);
+	}
+
+	ul {
+		list-style: none;
+		padding: 5px;
+	}
+
+	li {
+		padding: 5px;
+		cursor: pointer;
+
+		display: flex;
+		align-items: center;
+		border-radius: 10px;
+
+		&:hover,
+		&.active {
+			background-color: rgb(228, 228, 250);
+		}
+
+		svg {
+			margin-right: 5px;
+		}
+	}
+`
