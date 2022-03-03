2f370d4a6425ccba5bb816762e78957d7ee1250e
deepzS2 <alanbackman10@hotmail.com>
2022-03-03 15:35:52 -0300

    💄(frontend): Adding new theme colors

--- frontend/src/@types/styled.d.ts
+++ frontend/src/@types/styled.d.ts
@@ -10,6 +10,8 @@ declare module 'styled-components' {
 			tertiary: string
 			white: string
 			red: string
+			grey: string
+			green: string
 		}
 	}
 }
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -8,6 +8,8 @@ const theme: DefaultTheme = {
 		tertiary: '#fafafa',
 		white: '#ffffff',
 		red: '#ff0000',
+		grey: '#555555',
+		green: '#00ff00',
 	},
 }
 
