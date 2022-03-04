ab3ac54c1f72f809babb84b18eecb152314eaa29
deepzS2 <alanbackman10@hotmail.com>
2022-03-04 12:26:58 -0300

    💄(frontend): Updating theme

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -10,6 +10,20 @@ const theme: DefaultTheme = {
 		red: '#ff0000',
 		grey: '#555555',
 		green: '#00ff00',
+		lightGreen: '#3bb077',
+		lighterGrey: '#eeeef7',
+		status: {
+			background: {
+				approved: '#e5faf2',
+				declined: '#fff0f1',
+				pending: '#ebf1fe',
+			},
+			color: {
+				approved: '#3bb017',
+				declined: '#d95087',
+				pending: '#2a7ade',
+			},
+		},
 	},
 }
 
