82aed99d9f3509384520e5ea3790c7a881459e0c
deepzS2 <alanbackman10@hotmail.com>
2022-03-07 13:45:39 -0300

    💄(frontend): Source Sans Pro font

--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -5,8 +5,7 @@ export default createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Source Sans Pro', sans-serif;
   }
 
   a {
